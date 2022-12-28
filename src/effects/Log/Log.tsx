5425bf64e8cf06a3df2dcb9e033d2023fe4be120
etouraille <eouard.touraille@gmail.com>
2022-12-28 10:18:52 +0100

    [] fixe effct Log

--- src/effects/Log/Log.tsx
+++ src/effects/Log/Log.tsx
@@ -49,6 +49,25 @@ const Log   = (props: LogProps) => {
                 // @ts-ignore
 
             }
+            if(document.getElementById('google-jssdk')) {
+                // @ts-ignore
+                google.accounts.id.initialize({
+                    client_id: props.client_id,
+                    callback: handleCredentialResponse.bind(this),
+                    auto_select: true,
+                    cancel_on_tap_outside: false,
+                });
+
+            }
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement('script');
+            js.id = id;
+            js.src = "https://accounts.google.com/gsi/client?onload=googleSDKLoaded";
+            fjs?.parentNode?.insertBefore(js, fjs);
+        }(document, 'script', 'google-jssdk'));
 
 
     }, [props.client_id]);
